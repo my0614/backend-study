11995af1d8dcb140a2346830ee29651eac9a1b49
my0614 <aqi222@naver.com>
2026-04-02 10:41:18 +0900

    schemas Response 스키마 Config 추가 및 구조 수정

--- Todo/my_app/todos/schemas.py
+++ Todo/my_app/todos/schemas.py
@@ -1,15 +1,41 @@
 # todos/schemas.py
 from pydantic import BaseModel
+from typing import List  
 
+class TodoObject(BaseModel):
+    title: str
+    description: str
+    priority: str
+    due_date: str
+    
 class CreateTodoRequest(BaseModel):
     title: str
     description: str
     priority: str
     due_date: str
 
-class TodoListResponse(BaseModel):
+class TodoListRequest(BaseModel):
     is_completed: bool
     priority: str
 
+class TodoRequest(BaseModel):
+    id: int
+
+class UpdateTodoRequest(BaseModel):
+    is_completed: bool
+    priority: str
+
+class DeleteTodoRequest(BaseModel):
+    id: int
+
+class CreateTodoResponse(BaseModel):
+    TodoObject
+    
     class Config:
-        from_attributes = True  # SQLAlchemy 모델 → Pydantic 변환 허용
+        from_attributes = True
+
+class TodoListResponse(BaseModel):
+    todos: List[TodoObject]
+    
+    class Config:
+        from_attributes = True
