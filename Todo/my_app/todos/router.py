6d2989b5d0cc683a5cfd344c50ea8768ac0af6b8
my0614 <aqi222@naver.com>
2026-04-02 11:34:39 +0900

    POST /todos 엔드포인트 추가 및 중복 제거

--- Todo/my_app/todos/router.py
+++ Todo/my_app/todos/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from todos.dependencies import get_todo_service
-from todos.schemas import CreateTodoRequest, TodoResponse
+from todos.schemas import CreateTodoRequest, TodoResponse, CreateTodoResponse
 from todos.service import TodoService
 
 router = APIRouter(prefix="/todos", tags=["todos"])
@@ -10,6 +10,12 @@ router = APIRouter(prefix="/todos", tags=["todos"])
 def get_all_todo(service: TodoService = Depends(get_todo_service)):
     return service.get_all_todos()
 
+#GET /todos/{todo_id}
 @router.get("/{todo_id}", response_model=TodoResponse)
 def get_todo(todo_id: int, service: TodoService = Depends(get_todo_service)):
     return service.get_todo(todo_id)
+
+#POST /todos
+@router.post("", response_model=CreateTodoResponse)
+def create_todo(request: CreateTodoRequest, service: TodoService = Depends(get_todo_service)):
+    return service.create_todo(request)
